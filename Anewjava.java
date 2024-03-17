89b8cb8c8f8e21641aac83604be840c65a537666
abdllwhbl <abdllwhbl@gmail.com>
2024-03-17 21:34:02 +0500

    java changes

--- Anewjava.java
+++ Anewjava.java
@@ -3,6 +3,8 @@ public  package newjava;
 class Anewjava {
     public static void main(String[] args) {
         System.out.println("sdfsdf");
+        System.out.println("new dfs");
+        
     }
 
     

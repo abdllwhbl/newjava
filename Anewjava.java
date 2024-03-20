97085f85795beedf14841897ffac535ab6ccad66
abdllwhbl <abdllwhbl@gmail.com>
2024-03-20 14:52:10 +0500

    fdsfsdfsdf

---
+++ Anewjava.java
@@ -0,0 +1,11 @@
+public  package newjava;
+
+class Anewjava {
+    public static void main(String[] args) {
+        System.out.println("sdfsdf");
+        System.out.println("new dfs");
+        System.out.println("new dfs");
+    }
+
+    
+}
